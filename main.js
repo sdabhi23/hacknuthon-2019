89291f4bfbe2a866a292956665b84f1822ed910b
Shrey <shrey.dabhi23@gmail.com>
2019-02-28 13:09:21 +0530

    update description and problem statements

---
+++ main.js
@@ -0,0 +1,59 @@
+const problems = {
+  defence: [
+    "Audio scrambling based encoding algorithm",
+    "Video stabilization for unmanned aerial vehicles"
+  ],
+  travel: [
+    "AR/VR based travel brochures",
+    "Network connectivity for remote locations",
+    "Virtual guides",
+    "Using artificial intelligence for a personalized travel experience"
+  ],
+  healthcare: [
+    "AI-based systems to detect diseases in their early stages",
+    "Identifying symptoms based on Image processing",
+    "Hygiene measuring devices",
+    "IoT based Emergency response system"
+  ],
+  agriculture: [
+    "A digitalised agri-supply chain",
+    "Automation in agriculture",
+    "A GIS (geographic information system) surveying/mapping for precision agriculture",
+    "Data-driven farming using Sensors on Crops and Farm Machinery"
+  ],
+  industry: [
+    "IoT based product monitoring system",
+    "Using deep learning algorithms to detect product details from packaging label",
+    "IoT based logistics management"
+  ],
+  education: [
+    "Digital portfolios to measure a child's progress",
+    "Using AR/VR to create interactive material",
+    "Simulation-based training programs",
+    "Adaptive virtual labs using AI"
+  ]
+};
+
+const titles = {
+  travel: "Travel / Tourism",
+  education: "Education",
+  agriculture: "Agriculture",
+  industry: "Industrial Automation",
+  healthcare: "Healthcare",
+  defence: "Defence"
+};
+
+$("#track_modal").on("show.bs.modal", function(event) {
+  var button = $(event.relatedTarget);
+  var name = button.data("track");
+  var modal = $(this);
+  var content = `<h5>Suggested problem statements:</h5>\n<br/>\n<ol>\n`;
+  problems[name].forEach(elem => {
+    content = content + `<li>${elem}</li>\n`;
+  });
+  content =
+    content +
+    `</ol>\n<p>There are just a few suggested problem statements for your refrence. We would suggest you to identify and resolve any problem you come across or feel is relevant in this domain.</p>`;
+  modal.find(".modal-title").text(titles[name]);
+  modal.find(".modal-body").html(content);
+});
